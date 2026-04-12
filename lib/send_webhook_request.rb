f378ada2f9c13fbc3a450ca14c5941e1d1724a75
Dhanush S <dhanush6102002@gmail.com>
2026-04-12 11:10:05 +0530

    feat: implement submission status views, webhook jobs, and completion UI component

--- lib/send_webhook_request.rb
+++ lib/send_webhook_request.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SendWebhookRequest
-  USER_AGENT = 'DocuSeal.com Webhook'
+  USER_AGENT = 'ArtisHorizon.com Webhook'
 
   LOCALHOSTS = DownloadUtils::LOCALHOSTS
 
